8555feddfcd4ce03212690a73e9720bea24014a7
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-03-29 20:25:58 +0530

    "Components Created"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import Grid from './Components/Grid'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      generation:0
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>The Game Of Life</h1>
+        {/* All the components will come here */}
+        <Grid />
+        <h1>{this.state.generation}</h1>
+      </div>
+
+
+
+
+
+    )
+  }
+
+
+
 }
 
 export default App;
